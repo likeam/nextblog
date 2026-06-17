3fadc09bcc958df19ab8f87d60a2fe0338cc6571
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-06-17 12:25:11 +0500

    Building Home Page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    browserToTerminal: true,
+    // 'error' — errors only (default)
+    // 'warn'  — warnings and errors
+    // true    — all console output
+    // false   — disabled
+  },
 };
 
 export default nextConfig;
