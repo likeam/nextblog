a5dfd733539db7e76f6e240c259aa32809165416
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-06-22 23:27:07 +0500

    Creating Posts Page

---
+++ app/posts/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PostPage = () => {
+  return (
+    <div className=" space-y-8">
+      <section className=" space-y-4">
+        <h1 className=" text-center text-4xl font-semibold text-zinc-950 sm:text-5xl">
+          Posts
+        </h1>
+      </section>
+    </div>
+  );
+};
+
+export default PostPage;
